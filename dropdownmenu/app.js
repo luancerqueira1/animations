5bffd3e9888033eec7b1176c6df73fecfe4f8b3e
Luan Cerqueira <luangamacerqueira1@gmail.com>
2022-10-05 17:43:48 -0300

    dropdown added

---
+++ dropdownmenu/app.js
@@ -0,0 +1,17 @@
+
+document.addEventListener('click', function (e) {
+   const isDropdownButton = e.target.matches('[data-dropdown-button]');
+   if (!isDropdownButton && e.target.closest('[data-dropdown]') != null) {
+      return
+   }
+   let currentDropdown;
+   if (isDropdownButton) {
+      currentDropdown = e.target.closest('[data-dropdown]');
+      currentDropdown.classList.toggle('active')
+   }
+
+   document.querySelectorAll('[data-dropDown].active').forEach(dropdown => {
+      if (dropdown === currentDropdown) return
+      dropdown.classList.remove('active')
+   });
+});
